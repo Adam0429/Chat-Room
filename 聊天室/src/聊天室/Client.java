46323081f34cd896ec406dabd68fadec02783765
wfh0429 <872490934@qq.com>
2017-03-31 23:36:03 +0800

    2017.3.31
    readline()啊！必须用flush或者writer.close();才能传走数据，要不然缓冲区满了他才传

--- 聊天室/src/聊天室/Client.java
+++ 聊天室/src/聊天室/Client.java
@@ -3,6 +3,8 @@ package 聊天室;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 public class Client {
@@ -14,11 +16,12 @@ public class Client {
 	}
 	public void go(){
 		try{
-			Socket s=new Socket("localhost",8888);//这里是要获取与服务器端口的连接，所以要先运行服务器程序
-			InputStreamReader is=new InputStreamReader(s.getInputStream());
+			Socket s=new Socket("localhost",8887);//这里是要获取与服务器端口的连接，所以要先运行服务器程序	
+			InputStreamReader is=new InputStreamReader(s.getInputStream());			
 			BufferedReader b=new BufferedReader(is);
+			PrintWriter pw=new PrintWriter(s.getOutputStream());
 			System.out.println(b.readLine());
-			b.close();
+			//System.out.println(b.readLine());//一直【等待写数据】，但又没有数据写入，所以就卡住了。
 			/*
 			 * 字节流读取单位为一个字节，字符流读取单位为一个字符 所以读取汉字的时候，如果用字节流就会导致读出来乱码
 			 * 所以这里不用stream而用reader。但读进来的是stream所以要用InputStreamReader转换成字符型
--- 聊天室/src/聊天室/Server.java
+++ 聊天室/src/聊天室/Server.java
@@ -1,10 +1,14 @@
 package 聊天室;
 
+import java.io.BufferedInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.io.Writer;
 import java.net.ServerSocket;
 import java.net.Socket;
+
+import javax.swing.JOptionPane;
 
 public class Server {
 	public static void main(String[] Args){
@@ -15,11 +19,14 @@ public class Server {
 	}
 	public void go(){
 		try{
-			ServerSocket ss=new ServerSocket(8888);//ServerSocket会监听客户端对这台机器在8888端口上的要求
+			ServerSocket ss=new ServerSocket(8887);//ServerSocket会监听客户端对这台机器在8888端口上的要求
 			while(true){
 				Socket s=ss.accept();//accept会停下来等到要求到达才会继续
+				InputStreamReader is=new InputStreamReader(new BufferedInputStream(s.getInputStream()));
 				PrintWriter w=new PrintWriter(s.getOutputStream());//Java.io.PrintWriter 类打印格式化对象的表示到文本输出流。
-				w.println("你好");
+				JOptionPane.showMessageDialog(null, "连接成功！");
+				w.println("你好");//bufferedreader类的必须要回车和flush才能接受
+				w.close(); //must have THIS or flush() or it never writes...
 			}
 		}
 		catch(IOException e){
