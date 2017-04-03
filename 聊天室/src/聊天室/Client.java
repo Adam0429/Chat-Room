341ecc97a7aa3f55b0ab65eea6f8c6e1f32d023c
wfh0429 <872490934@qq.com>
2017-04-03 16:14:21 +0800

    2017.4.3
    ui写好了,服务器和客户端的传输还有问题.还有怎么使用户点对点的传输是个问题

--- 聊天室/src/聊天室/Client.java
+++ 聊天室/src/聊天室/Client.java
@@ -1,5 +1,7 @@
 package 聊天室;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -7,21 +9,48 @@ import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
 public class Client {
+	JFrame f;
+	JTextField tf;
+	JTextArea ta;
+	JButton b;
+	BufferedReader br;
+	PrintWriter pw;
 	public static void main(String[] Args){
 		new Client().go();
+		
 	}
 	public Client(){
-	
+		f=new JFrame("聊天室");
+		tf=new JTextField();
+		ta=new JTextArea();
+		b=new JButton("发送");
+		f.setSize(450, 400);
+		f.setLayout(null);
+		b.setBounds(270,300,90,30);
+		b.addActionListener(new MyButton());
+		ta.setBounds(50,50,300,230);
+		tf.setBounds(50,300,200,20);
+		f.add(tf);
+		f.add(ta);
+		f.add(b);
+		f.setVisible(true);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 	public void go(){
 		try{
 			Socket s=new Socket("localhost",8887);//这里是要获取与服务器端口的连接，所以要先运行服务器程序	
 			InputStreamReader is=new InputStreamReader(s.getInputStream());			
-			BufferedReader b=new BufferedReader(is);
-			PrintWriter pw=new PrintWriter(s.getOutputStream());
-			System.out.println(b.readLine());
-			//System.out.println(b.readLine());//一直【等待写数据】，但又没有数据写入，所以就卡住了。
+			br=new BufferedReader(is);
+			pw=new PrintWriter(s.getOutputStream());
+			Thread readthread=new Thread(new incomingReader());
+			readthread.start();
 			/*
 			 * 字节流读取单位为一个字节，字符流读取单位为一个字符 所以读取汉字的时候，如果用字节流就会导致读出来乱码
 			 * 所以这里不用stream而用reader。但读进来的是stream所以要用InputStreamReader转换成字符型
@@ -31,5 +60,29 @@ public class Client {
 			e.printStackTrace();
 		}
 	}
+	public class MyButton implements ActionListener{
+
+		public void actionPerformed(ActionEvent arg0) {
+			String string=tf.getText();
+			pw.println(string);
+			pw.flush();
+			tf.setText("");
+			tf.requestFocus();//光标进入这个控件中
+		}
+	}
 	
+	public class incomingReader implements Runnable{
+		public void run() {
+			String message;             
+	           try {
+	             while ((message = br.readLine()) != null) {                        
+	                System.out.println("read " + message);
+	                ta.append(message + "\n");
+	             }
+	           }
+	           catch(Exception ex) {
+	        	   ex.printStackTrace();
+	           }
+		}
+	}
 }
--- 聊天室/src/聊天室/Server.java
+++ 聊天室/src/聊天室/Server.java
@@ -1,6 +1,7 @@
 package 聊天室;
 
 import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
@@ -9,8 +10,11 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 import javax.swing.JOptionPane;
+import javax.swing.text.AbstractDocument.BranchElement;
 
 public class Server {
+	BufferedReader br;
+	PrintWriter w;
 	public static void main(String[] Args){
 		new Server().go();
 	}
@@ -22,15 +26,36 @@ public class Server {
 			ServerSocket ss=new ServerSocket(8887);//ServerSocket会监听客户端对这台机器在8888端口上的要求
 			while(true){
 				Socket s=ss.accept();//accept会停下来等到要求到达才会继续
-				InputStreamReader is=new InputStreamReader(new BufferedInputStream(s.getInputStream()));
-				PrintWriter w=new PrintWriter(s.getOutputStream());//Java.io.PrintWriter 类打印格式化对象的表示到文本输出流。
+				InputStreamReader is=new InputStreamReader(s.getInputStream());
+				br=new BufferedReader(is);
+				w=new PrintWriter(s.getOutputStream());//Java.io.PrintWriter 类打印格式化对象的表示到文本输出流。
 				JOptionPane.showMessageDialog(null, "连接成功！");
-				w.println("你好");//bufferedreader类的必须要回车和flush才能接受
-				w.close(); //must have THIS or flush() or it never writes...
+				String string="--------已进入聊天室--------";
+				w.println(string);//bufferedreader类的必须要回车和flush或close才能接受
+				w.flush();
+				Thread managerthread=new Thread(new manager()); 
+				managerthread.run();
 			}
 		}
 		catch(IOException e){
 			e.printStackTrace();
+		}
+		
+	}
+	
+	public class manager implements Runnable{
+		String message;
+		public void run() {
+			try{
+				while((message=br.readLine())!=null){
+					System.out.println(message);
+					w.write(message);
+					w.flush();
+				}
+			}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
 		}
 	}
 }
