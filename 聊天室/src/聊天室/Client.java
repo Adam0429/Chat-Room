ed1cc6652982d1af8a2692224fd4bb43004b9192
wfh0429 <872490934@qq.com>
2017-11-17 23:15:54 +0800

    2017.11.17
    下一个写list

--- 聊天室/src/聊天室/Client.java
+++ 聊天室/src/聊天室/Client.java
@@ -10,9 +10,7 @@ import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
-import java.util.HashMap;
 
-import javax.security.auth.callback.TextOutputCallback;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -35,63 +33,73 @@ public class Client {
 	BufferedReader br;
 	PrintWriter pw;
 	Socket s;
-	String Serverip;
-	private JTextField tf2;
-	private JTextField textField_1;
+	static String name = "null";
 	public static void main(String[] Args){
-		new Client().go();
+		new Client(name).go();
 		
 	}
-	public Client(){
+	public Client(String n){
+		name = n;
 		f=new JFrame("聊天室");
 		tf=new JTextField();
 		ta=new JTextArea(10,20);
-		b=new JButton("\u7FA4\u53D1");
-		b2=new JButton("\u79C1\u804A");
+		b=new JButton("BROADCAST ");
+		b2=new JButton("STATS ");
 		ta.setLineWrap(true);//激活自动换行功能 			
         ta.setWrapStyleWord(true);// 激活断行不断字功能	
         ta.setEditable(false);	
         qScroller = new JScrollPane(ta);
         qScroller.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
         qScroller.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-		f.setSize(450, 450);
+		f.setSize(404, 521);
 		f.getContentPane().setLayout(null);
-		b.setBounds(273,340,90,30);
-		b2.setBounds(273,307,90,30);
+		b.setBounds(50,338,152,30);
+		b2.setBounds(50,425,310,30);
 		b.addActionListener(new MyButton());
 		b2.addActionListener(new MyButton2());
-		qScroller.setBounds(14,50,278,230);
+		qScroller.setBounds(50,50,300,230);
 		//ta.setBounds(50,50,300,230);	
-		tf.setBounds(56,345,200,20);
+		tf.setBounds(50,293,287,20);
 		f.getContentPane().add(qScroller);
 		f.getContentPane().add(tf);
 		f.getContentPane().add(b);
 		f.getContentPane().add(b2);
 		
-		tf2 = new JTextField();
-		tf2.setBounds(78, 312, 178, 20);
-		f.getContentPane().add(tf2);
+		JButton btnStop = new JButton("STOP");
+		btnStop.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {
+					pw.println(name+"退出");
+					pw.flush();
+					s.close();
+					JOptionPane.showMessageDialog(null, "断开连接");
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+		});
+		btnStop.setBounds(216, 340, 121, 27);
+		f.getContentPane().add(btnStop);
 		
-		textField_1 = new JTextField();
-		textField_1.setText("无用户");
-		textField_1.setBounds(306, 50, 112, 238);
-		f.getContentPane().add(textField_1);
-		textField_1.setColumns(10);
+		JButton btnList = new JButton("LIST");
+		btnList.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//
+			}
+		});
+		btnList.setBounds(50, 381, 152, 27);
+		f.getContentPane().add(btnList);
 		
-		JLabel lblNewLabel = new JLabel("\u79C1\u804A\u5BF9\u8C61:");
-		lblNewLabel.setBounds(0, 313, 72, 18);
-		f.getContentPane().add(lblNewLabel);
-		
-		JLabel label = new JLabel("\u5185\u5BB9\uFF1A");
-		label.setBounds(0, 346, 72, 18);
-		f.getContentPane().add(label);
+		JButton btnNewButton = new JButton("KICK");
+		btnNewButton.setBounds(216, 380, 121, 27);
+		f.getContentPane().add(btnNewButton);
 		f.setVisible(true);
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 	public void go(){
-		Serverip = "127.0.0.1";
 		try{
-			s=new Socket(Serverip, 8888);//这里是要获取与服务器端口的连接，所以要先运行服务器程序	
+			s=new Socket("localhost", 8888);//这里是要获取与服务器端口的连接，所以要先运行服务器程序	
 			InputStreamReader is=new InputStreamReader(s.getInputStream());			
 			br=new BufferedReader(is);
 			pw=new PrintWriter(s.getOutputStream());
@@ -111,10 +119,10 @@ public class Client {
 
 		public void actionPerformed(ActionEvent arg0) {
 			String string=tf.getText();
-			String from;
+			String ip;
 			try {
-				from = InetAddress.getLocalHost().getHostAddress();
-				pw.println(from+"说:"+string);
+				ip = InetAddress.getLocalHost().getHostAddress();
+				pw.println(ip+"说:"+string);
 				pw.flush();
 				tf.setText("");
 				tf.requestFocus();//光标进入这个控件中
@@ -128,12 +136,10 @@ public class Client {
 	public class MyButton2 implements ActionListener{
 
 		public void actionPerformed(ActionEvent arg0) {
-			String string=tf.getText();
-			String to=tf2.getText();
-			String from;
+			String ip;
 			try {
-				from = InetAddress.getLocalHost().getHostAddress();
-				pw.println(from+"aaa"+string+"aaa"+to);//到server里check一下是私聊还是广播
+				ip = InetAddress.getLocalHost().getHostAddress();
+				pw.println(ip+"说:"+"我错了");
 				pw.flush();
 				tf.requestFocus();//光标进入这个控件中
 			} catch (UnknownHostException e) {
@@ -145,23 +151,15 @@ public class Client {
 	public class incomingReader implements Runnable{
 		public void run() {
 			String message;             
-			try {
+	           try {
 	             while ((message = br.readLine()) != null) { 
 	            	//readline读到换行符才算读到一行,而且还需要flush()或close()。因为输入流缓冲区不满，他是不会接收到数据的
 	            	//System.out.println("read " + message);
-	            	if(message.contains("IPlist")){
-	            		System.out.println(message);
-
-	            		String iplist = message.replace("127.0.0.1",Serverip);
-	            		textField_1.setText(iplist);
-	            	}
-	            	else{
-	            		ta.append(message + "\n");
-	                	int height=15;								//自动换行
-	                	Point p = new Point();
-	                	p.setLocation(0,ta.getLineCount()*height);
-	                	qScroller.getViewport().setViewPosition(p);
-	            	}
+	                ta.append(message + "\n");
+	                int height=15;								//自动换行
+	                Point p = new Point();
+	                p.setLocation(0,ta.getLineCount()*height);
+	                qScroller.getViewport().setViewPosition(p);
 	             }
 	           }
 	           catch(Exception e) {
--- 聊天室/src/聊天室/Server.java
+++ 聊天室/src/聊天室/Server.java
@@ -1,136 +1,65 @@
 package 聊天室;
 
 import java.awt.BorderLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.BufferedReader;
-import java.io.File;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map.Entry;
-import java.util.PrimitiveIterator.OfDouble;
 
+import javax.print.attribute.standard.Severity;
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JList;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-import javax.swing.JTextField;
-import javax.swing.text.GapContent;
-import javax.xml.soap.Text;
-
-import org.omg.CORBA.PUBLIC_MEMBER;
-
-import javax.swing.JScrollBar;
-import javax.swing.JTextArea;
-import java.awt.List;
-import java.awt.TextArea;
-
-
 public class Server {
-	File file;
-	HashMap<String, PrintWriter> clientOutputStreams;
-	ArrayList<String> clients;
-	int count;
-	JTextField textField_1;
-    TextArea textArea;
-	JFrame jf;
-	JPanel panel;
-	String IPlist;
-	JButton b;
-	
+	HashMap<String,PrintWriter> clientOutputStreams;
 	public static void main(String[] Args){
 		Server server=new Server();
-		//server.go();
 	}
 	public Server(){
-		IPlist ="";
-		clients = new ArrayList<String>();
-
-		jf=new JFrame("Server");
-		panel=new JPanel();
-		b=new JButton("启动");
-		b.setBounds(42, 43, 63, 27);
-		b.addActionListener(new blistener2());
-		panel.setLayout(null);
-		panel.add(b);
+		JFrame jf=new JFrame("Server");
+		JPanel panel=new JPanel();
+		JPanel panel2=new JPanel();
+		JButton b=new JButton("启动");//因为启动执行的是线程,线程会一直跑,所以按钮会卡住
+		JButton b2=new JButton("停止");//因为启动会卡主，所以停止也不能用了，放这里装样子
+		b.addActionListener(new blistener());
+		panel.add(BorderLayout.CENTER,b);
+		panel.add(BorderLayout.CENTER,b2);
 		jf.getContentPane().add(BorderLayout.CENTER,panel);
-		JButton btnNewButton = new JButton("Kick");
-		btnNewButton.setBounds(31, 201, 139, 27);
-		btnNewButton.addActionListener(new blistener());
-		panel.add(btnNewButton);
-		
-		textField_1 = new JTextField();
-		textField_1.setBounds(31, 151, 139, 24);
-		textArea = new TextArea();
-		textArea.setBounds(212, 33, 140, 252);
-		panel.add(textField_1);
-		textField_1.setColumns(10);
-		
-		JLabel lblNewLabel = new JLabel("IP online");
-		lblNewLabel.setBounds(216, 13, 72, 18);
-		panel.add(lblNewLabel);
-		
-		
-		panel.add(textArea);
+		jf.getContentPane().add(BorderLayout.EAST, panel2);
 		jf.setVisible(true);
-		jf.setSize(400, 342);
+		jf.setSize(400, 80);
 		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		count=0;
 		go();
-
 	}
-	
-	public void updatelist(){
-		IPlist = "";
-		Iterator<String> i = clients.iterator();
-
-		while(i.hasNext()){
-		
-			IPlist = IPlist + i.next()+'\n';
-	
-		}
-		textArea.setText(IPlist);
-		
-	}
-	
 	public void go(){
-
-		clientOutputStreams = new HashMap<String, PrintWriter>();			
+		clientOutputStreams = new HashMap<String,PrintWriter>();
 		try{
 			ServerSocket ss=new ServerSocket(8888);//ServerSocket会监听客户端对这台机器在8888端口上的要求
-			Socket s;
 			while(true){
-				s=ss.accept();//accept会停下来等到要求到达才会继续.
-				
-				System.out.println("加进来了一个");
+				Socket s=ss.accept();//accept会停下来等到要求到达才会继续.
 				PrintWriter w=new PrintWriter(s.getOutputStream());
-				String ip = s.getInetAddress().toString().split("/")[1];
-				if(clients.contains(ip)){
-					System.out.println("已连入相同ip");	
-				}
-				else{
-				clientOutputStreams.put(ip,w);
+				clientOutputStreams.put("Client"+clientOutputStreams.size(),w);
 				//JOptionPane.showMessageDialog(null, "连接成功！");
-				tellEveryone(s.getInetAddress().toString().split("/")[1]+"加入群聊");
-				w.flush();
-				HandleAClient hc = new HandleAClient(s);
-				clients.add(s.getInetAddress().toString().split("/")[1]);
-				updatelist();
-				tellonlinelist(IPlist);
-				tellEveryone("IPlist:"+IPlist);
-				Thread thread=new Thread(hc); 
-				thread.start();//如果有新的socket加进来，就为其多开一个线程
-				}
-				
-			}
+				w.println(s.getInetAddress()+"加入群聊");
+				w.println(clientOutputStreams.size());
+				w.println("Client"+clientOutputStreams.size());
 
+				w.flush();
+				Thread thread=new Thread(new HandleAClient(s)); 
+				thread.start();//如果有新的socket加进来，就为其多开一个线程
+			}
 		}
 		catch(Exception e){
 			e.printStackTrace();
@@ -139,30 +68,18 @@ public class Server {
 	
 	public class blistener implements ActionListener{	
 		public void actionPerformed(ActionEvent e) {
-			String ip = textField_1.getText();
-			PrintWriter p=clientOutputStreams.get(ip);
-			tellEveryone(ip+"被踢了");
-			p.close();
-			clientOutputStreams.remove(ip);
-			clients.remove(ip);
-			updatelist();
-			textArea.setText(IPlist);
-//			System.out.println(clients.size());
-			tellonlinelist(IPlist);
+			go();
 			
 		}
 		
 	}
 	
-	public class blistener2 implements ActionListener{	
+	public class blistener2 implements ActionListener{
 		public void actionPerformed(ActionEvent e) {
-			tellEveryone("dsdadsa");
+			//go();
 		}
 		
 	}
-
-		
-	
 	public class HandleAClient implements Runnable{
 		Socket s;
 		BufferedReader br;
@@ -181,19 +98,8 @@ public class Server {
 			try{
 				//bufferedreader类的必须要回车和flush或close才能接受
 				while((message=br.readLine())!=null){
-					/*实际上readLine()是一个阻塞函数，当没有数据读取时，就一直会阻塞在那，而不是返回null
-					readLine()只有在数据流发生异常或者另一端被close()掉时，才会返回null值*/
-					if(message.contains("aaa")){//用来check是不是私聊
-						String from=message.split("aaa")[0];
-						String strings=message.split("aaa")[1];
-						String to=message.split("aaa")[2];
-						System.out.println(from+"对"+to+"说"+strings);
-						tellSomeone(strings,from ,to);
-					}
-					else{
-						System.out.println(message);
-						tellEveryone(message);
-					}
+					System.out.println(message);
+					tellEveryone(message);
 				}
 			}
 			catch (Exception e) {
@@ -201,54 +107,21 @@ public class Server {
 			}
 		}
 		
-		public void tellSomeone(String message, String from, String to){
-			try {
-				PrintWriter fromwriter=clientOutputStreams.get(from);
-				PrintWriter towriter= clientOutputStreams.get(to);
-				
-				towriter.println("from say:"+message);
-				
-				towriter.flush();
-				
-				fromwriter.println("say to "+to+" "+message);
-				fromwriter.flush();
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-			
-		}
-	}
-		
-		public void tellEveryone(String message) {
-		     Iterator it = clientOutputStreams.entrySet().iterator();		     
-		     while(it.hasNext()) {
-		    	 try {
-		             Entry entry = (Entry)it.next();
-		             PrintWriter writer = new PrintWriter((PrintWriter) entry.getValue());//Java.io.PrintWriter 类打印格式化对象的表示到文本输出流。
-					 writer.println(message);
-		             writer.flush();
-		         }  
-		         catch(Exception ex) {
-		        	 ex.printStackTrace();
+		 public void tellEveryone(String message) {
+		      Iterator it = clientOutputStreams.entrySet().iterator();
+		      while(it.hasNext()) {
+		        try {
+		        	Entry entry = (Entry) it.next();
+		        	PrintWriter writer = (PrintWriter) entry.getValue();//Java.io.PrintWriter 类打印格式化对象的表示到文本输出流。
+		            writer.println(message);
+		            writer.flush();
+		        }  
+		        catch(Exception ex) {
+		        	ex.printStackTrace();
 		         }
-		     }
 		      
-		   } // end while
+		       } // end while
 		       
-		public void tellonlinelist(String iplist){
-			  Iterator it = clientOutputStreams.entrySet().iterator();		     
-			     while(it.hasNext()) {
-			    	 try {
-			             Entry entry = (Entry)it.next();
-			             PrintWriter writer = new PrintWriter((PrintWriter) entry.getValue());//Java.io.PrintWriter 类打印格式化对象的表示到文本输出流。
-						 writer.println("IPlist:"+IPlist);
-			             writer.flush();
-			         }  
-			         catch(Exception ex) {
-			        	 ex.printStackTrace();
-			         }
-			
-		}
-		   // close tellEveryone
+		   } // close tellEveryone
 	}
 }
